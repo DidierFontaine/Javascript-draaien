755097154cd1df617dde5c23314f258e5306a083
DidierFontaine <71550187+DidierFontaine@users.noreply.github.com>
2020-11-09 15:54:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy studenten")
+//  Dit is een grote som.
+/*const number=1230941
+const number1=1823794
+console.log(number * number1)
+const number2=637263
+const number3=54
+console.log(number2/number3)*/
